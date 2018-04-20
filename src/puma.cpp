f30c2a44e91c29f62e80b91723cd4aa0184c9c7d
Darek Dulka <goloman@onet.pl>
2018-04-20 19:00:39 +0200

    shader compilation

--- src/puma.cpp
+++ src/puma.cpp
@@ -1,9 +1,7 @@
 #include "puma.hpp"
 
-#include <GL/glew.h>
-#include <SDL2/SDL_opengl.h>
-
 #include <stdexcept>
+#include <fstream>
 
 void puma::Puma::run() {
     init();
@@ -14,29 +12,34 @@ void puma::Puma::run() {
 void puma::Puma::init() {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER)) {
         SDL_Log("%s", SDL_GetError());
-        std::runtime_error("SDL initialization failed");
+        throw std::runtime_error("SDL initialization failed");
     }
 
     window = SDL_CreateWindow("puma", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 1024, SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
     if (!window) {
         SDL_Log("%s", SDL_GetError());
-        std::runtime_error("Window creation failed");
+        throw std::runtime_error("Window creation failed");
     }
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0); //TODO https://wiki.libsdl.org/SDL_GLattr
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0); // TODO https://wiki.libsdl.org/SDL_GLattr
 
     context = SDL_GL_CreateContext(window);
     if (!context) {
         SDL_Log("%s", SDL_GetError());
-        std::runtime_error("Context creation failed");
+        throw std::runtime_error("Context creation failed");
     }
 
     // NOTE potentially needed?
     //glewExperimental = GL_TRUE
     GLenum glewRet = glewInit();
+
+    createShaderFromFile("resources/phongVS.glsl", GL_VERTEX_SHADER);
+    createShaderFromFile("resources/phongFS.glsl", GL_FRAGMENT_SHADER);
+
+    // TODO load shaders and create programs
 }
 
 void puma::Puma::loop() {
@@ -62,6 +65,35 @@ void puma::Puma::handleEvents() {
             break;
         }
     }
+}
+
+GLuint puma::Puma::createShaderFromFile(const char* filename, GLenum shaderType) {
+    GLuint shader = glCreateShader(shaderType);
+
+    std::ifstream f;
+    f.open(filename);
+
+    f.seekg(0, std::ios::end);
+    int length = f.tellg();
+    f.seekg(0, std::ios::beg);
+
+    char buffer[length];
+    f.read(buffer, length);
+    f.close();
+
+    char *start = buffer;
+    glShaderSource(shader, 1, &start, &length);
+    glCompileShader(shader);
+
+    int  success;
+    char log[1024];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        glGetShaderInfoLog(shader, 1024, NULL, log);
+        SDL_Log("%s:\n%s", filename, log);
+        throw std::runtime_error("Shader compilation failed");
+    }
+    return shader;
 }
 
 void puma::Puma::update() {
--- src/puma.hpp
+++ src/puma.hpp
@@ -3,6 +3,9 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
+#include <GL/glew.h>
+#include <SDL2/SDL_opengl.h>
+
 namespace puma {
 
 class Puma {
@@ -17,6 +20,8 @@ private:
     void handleEvents();
     void update();
     void render();
+
+    GLuint createShaderFromFile(const char* filename, GLenum shaderType);
 
     SDL_Window* window;
     SDL_GLContext context;
