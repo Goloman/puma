0a0a42eed67d28a08c4a2690432323699a217f8f
Darek Dulka <goloman@onet.pl>
2018-04-25 12:38:39 +0200

    line segment distance

--- resources/particleFS.glsl
+++ resources/particleFS.glsl
@@ -1,8 +1,16 @@
 #version 330 core
 
-in vec4 color;
+in float age;
+in vec2 uv;
+in float lineLength;
+
 out vec4 outColor;
 
 void main() {
-    outColor = color;
+    float r;
+    if (uv.x < 0) r = length(uv);
+    else if (uv.x > lineLength) r = length(uv - vec2(lineLength, 0));
+    else r = abs(uv.y);
+    r = floor(r * 2);
+    outColor = vec4(1-age, age, 1-r, 1-r);
 }
--- resources/particleGS.glsl
+++ resources/particleGS.glsl
@@ -11,32 +11,40 @@ in VertexData {
     float age;
 } gs_in[];
 
-out vec4 color;
+out float age;
+out vec2 uv;
+out float lineLength;
 
-float r = 0.002f;
-float len = 0.01f;
+float r = 0.01f;
+float len = 10.f;
 
-void createVertex(vec3 t, vec3 b, float l) {
+float maxAge = 2.f;
+
+void createVertex(vec3 t, vec3 b, float l, vec2 coords) {
     vec3 pos = vec3(gl_in[0].gl_Position);
     pos += t * r;
-    pos += t * l;
+    pos += t * l/2 * r;
     pos += b * r;
     gl_Position = projection * vec4(pos, 1);
-    color = vec4 (1 - gs_in[0].age / 2, gs_in[0].age / 2, 0, 1);
+    age = gs_in[0].age / maxAge;
+    uv = coords;
+    lineLength = l;
     EmitVertex();
 }
 
 void main() {
+    if (gs_in[0].age >= maxAge) return;
+
     vec3 tangent = normalize(gs_in[0].velocity);
     vec3 toCamera = vec3(-gl_in[0].gl_Position);
     vec3 binormal = normalize(cross(tangent, toCamera));
 
     float l = length(gs_in[0].velocity) * len;
 
-    createVertex(tangent, binormal, l);
-    createVertex(tangent, -binormal, l);
-    createVertex(-tangent, binormal, l);
-    createVertex(-tangent, -binormal, l);
+    createVertex(tangent, binormal, l, vec2(1+l, 1));
+    createVertex(tangent, -binormal, l, vec2(1+l, -1));
+    createVertex(-tangent, binormal, l, vec2(-1, 1));
+    createVertex(-tangent, -binormal, l, vec2(-1, -1));
 
     EndPrimitive();
 }
